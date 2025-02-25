64edcda62bd7b0618a5e5f9599cf9df67813dc56
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-02-25 18:40:25 -0300

    post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,38 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+          <div className={styles.info}>
+            <strong>Guilherme</strong>
+            <span>Desenvolvedor Front-end</span>
+          </div>
+        </div>
+        <time title="25 de fevereiro às 17:33" dateTime="2025-25-02 17:33:00">
+          Publicado à 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a>
+          <a href="#">#nlw</a>
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
