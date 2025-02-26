e886183152cb8769fb918ee3a3d1bcd7d4316d69
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-02-26 14:59:37 -0300

    comment list component

---
+++ src/components/Comment..jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.author}>
+              <strong>Guilherme</strong>
+              <time
+                title="26 de fevereiro às 14:33"
+                dateTime="2025-26-02 14:33:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito Bom, Devon! parabéns!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
