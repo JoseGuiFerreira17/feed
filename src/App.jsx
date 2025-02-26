4fa5ad258d9a795c744a4fdd057455504fda8dc3
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-02-26 16:32:01 -0300

    adding real information

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,52 @@ import './global.css';
 import styles from './App.module.css';
 
 export function App() {
+  const posts = [
+    {
+      id: 1,
+      author: {
+        avatarUrl:
+          'https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        name: 'Guilherme',
+        role: 'Desenvolvedor Front-end',
+      },
+      content: [
+        {
+          type: 'paragraph',
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        },
+        {
+          type: 'paragraph',
+          content:
+            'Vitae cumque nobis cupiditate dolore provident ratione porro rerum aliquid libero voluptas tenetur eius, amet nam soluta tempora quam nesciunt modi reprehenderit!',
+        },
+        { type: 'link', content: 'guilherme.com/portfolio' },
+      ],
+      publishedAt: new Date('2025-02-26T12:33:00'),
+    },
+    {
+      id: 2,
+      author: {
+        avatarUrl:
+          'https://images.unsplash.com/photo-1654110455429-cf322b40a906?q=50&w=600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        name: 'Diego Hernandez',
+        role: 'Desenvolvedor Fullstack',
+      },
+      content: [
+        {
+          type: 'paragraph',
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        },
+        {
+          type: 'paragraph',
+          content:
+            'Vitae cumque nobis cupiditate dolore provident ratione porro rerum aliquid libero voluptas tenetur eius, amet nam soluta tempora quam nesciunt modi reprehenderit!',
+        },
+        { type: 'link', content: 'diego.com/portfolio' },
+      ],
+      publishedAt: new Date('2025-02-26T14:43:00'),
+    },
+  ];
   return (
     <>
       <Header />
@@ -14,8 +60,16 @@ export function App() {
           <Sidebar />
         </aside>
         <main>
-          <Post author="Guilherme" content="Texto desse post" />
-          <Post author="Guilherme" content="Texto desse post" />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,36 +1,48 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment.';
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+  const publishedFormated = format(publishedAt, "d 'de' LLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+
+  const publishedRelativeNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.info}>
-            <strong>Guilherme</strong>
-            <span>Desenvolvedor Front-end</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="25 de fevereiro às 17:33" dateTime="2025-25-02 17:33:00">
-          Publicado à 1h
+        <time title={publishedFormated} dateTime={publishedAt.toISOString()}>
+          {publishedRelativeNow}
         </time>
       </header>
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          <a href="#">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="#">#novoprojeto</a>
-          <a href="#">#nlw</a>
-          <a href="#">#rocketseat</a>
-        </p>
+        {content.map((item, index) => {
+          if (item.type === 'paragraph') {
+            return <p key={index}>{item.content}</p>;
+          }
+          if (item.type === 'link') {
+            return (
+              <p key={index}>
+                <a href="#">{item.content}</a>
+              </p>
+            );
+          }
+          return null;
+        })}
       </div>
 
       <form className={styles.form}>
